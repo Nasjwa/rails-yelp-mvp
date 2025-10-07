ec4b207c014fcc676b61acbd54e50c1da0493d56
Nasjwa Eddyani <nasjwaeddyani@hotmail.com>
2025-10-07 12:52:38 +0200

    new resto

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning restaurants..."
+Restaurant.destroy_all
+
+puts "Seeding restaurants..."
+Restaurant.create!([
+  { name: "Epicure",      address: "75008 Paris",     phone_number: "+33 1 23 45 67 89", category: "french"  },
+  { name: "Sushi Zen",    address: "Shinjuku, Tokyo", phone_number: "+81 3 1234 5678",   category: "japanese"},
+  { name: "Pasta Basta",  address: "Rome, Italy",     phone_number: "+39 06 5555 1111",  category: "italian" },
+  { name: "Wok Star",     address: "Shanghai, China", phone_number: "+86 21 2222 3333",  category: "chinese" },
+  { name: "Frites & Co",  address: "Brussels, BE",    phone_number: "+32 2 999 8888",    category: "belgian" }
+])
+puts "Done!"
